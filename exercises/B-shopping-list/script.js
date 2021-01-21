afc822e8cfe0e8dc572d0cad470c1a8cb0bf04f2
Amanuel <weditedi25@gmail.com>
2021-01-21 18:52:13 +0000

    Task-B

--- exercises/B-shopping-list/script.js
+++ exercises/B-shopping-list/script.js
@@ -1,7 +1,15 @@
-function shoppingList(arrayOfPeople) {
+function shoppingList(arrList) {
   // Write your code here...
+  const content = document.getElementById('content');
+  const parenEl = document.createElement('ul');
+  content.appendChild(parenEl);
+  for( let list in arrList){
+    parenEl.appendChild(document.createElement('li')).innerHTML = `${arrList[list]}`;
+ }
+
 }
 
 let shopping = ["Milk", "Bread", "Eggs", "A Dinosaur", "Cake", "Sugar", "Tea"];
 
 shoppingList(shopping);
+
