d28ae5d2458051be86904f9b358646dabc0ee7b5
Amanuel <weditedi25@gmail.com>
2021-01-21 23:46:16 +0000

    Task-E

--- exercises/E-list-of-colours/script.js
+++ exercises/E-list-of-colours/script.js
@@ -1,5 +1,28 @@
 function listOfColours(colours) {
   // Write your code here...
+  const content = document.getElementById('content');
+  const txt = document.createElement('p');
+  txt.innerHTML = "Colors: ";
+  txt.className = "pos";
+  content.appendChild(txt);
+
+  const parentEl = document.createElement('select');
+  content.appendChild(parentEl);
+  
+
+  for( let i = 0; i < colours.length; i++){
+   parentEl.appendChild(document.createElement('option')).innerHTML = `${colours[i]}`
+  }
+
+  const colorOptions = document.getElementsByTagName("select");
+  for( let j =0; j < colorOptions.length; j++){
+
+    colorOptions[j].onclick = function() {
+    content.style.color = `${colours[j]}`;
+  }
+  
+   }
+
 }
 
 const colours = ["red", "blue", "green", "yellow", "pink", "brown"];
