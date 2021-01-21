99829248a023fc4f79d0ffa1d4f4f047ae4b7a6c
Amanuel <weditedi25@gmail.com>
2021-01-21 22:34:39 +0000

    Task D

--- exercises/D-todo-list/script.js
+++ exercises/D-todo-list/script.js
@@ -1,5 +1,22 @@
 function todoList(todos) {
   // Write your code here...
+  //creates ul tag and append it to the dic
+  const content = document.getElementById('content');
+  const parentEl = document.createElement('ul');
+  content.appendChild(parentEl);
+
+
+  for( let i = 0; i < todos.length; i++){
+   //create li ele, loops through the array and and assign the li value from array todos
+  parentEl.appendChild(document.createElement('li')).innerHTML = `${todos[i].todo}`
+  const lists = document.getElementsByTagName('li');
+  // toggles on click 
+    lists[i].onclick = function() {
+    lists[i].classList.toggle("listStyle");
+    }
+  }
+
+
 }
 
 const todos = [
