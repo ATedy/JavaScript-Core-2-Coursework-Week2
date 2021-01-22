5622c381b5859f9e9a0c152ae0e930bb6e3ad89d
Amanuel <weditedi25@gmail.com>
2021-01-22 19:48:30 +0000

    comment added

--- exercises/E-list-of-colours/script.js
+++ exercises/E-list-of-colours/script.js
@@ -30,55 +30,9 @@ const content = document.getElementById('content');
  })
 
 
-function colorChanger(colours){
-  for( let j = 0; j < colours.length; j++){
-    parentEl.options[j].style.color = `${parentEl.options[j].textContent}`;
-
-    }
-  }
-
-  parentEl.addEventListener('click', colorChanger);
-
-
 
 }
 
 const colours = ["red", "blue", "green", "yellow", "pink", "brown"];
 
 listOfColours(colours);
-
-
-// const content = document.getElementById('content');
-//   const txt = document.createElement('p');
-//   txt.innerHTML = "Colors: ";
-//   txt.className = "pos";
-//   content.appendChild(txt);
-
-//   const parentEl = document.createElement('select');
-//   content.appendChild(parentEl);
-  
-
-//   for( let i = 0; i < colours.length; i++){
-//    parentEl.appendChild(document.createElement('option')).innerHTML = `${colours[i]}`;
-
-//   }
-//  console.log( `${parentEl.options[0].textContent}`);
-
-
-// function colorChanger(colours){
-//   for( let j = 0; j < colours.length; j++){
-//     parentEl.options[j].style.color = `${parentEl.options[j].textContent}`;
-
-//     }
-//   }
-
-//   parentEl.addEventListener('click', colorChanger);
-
-
-
-// for( let j =0; j < colours.length; j++){
-
-//   parentEl.onclick = function() {
-//     parentEl.options[j].style.color = `${colours[j]}`;
-// }
-// }
